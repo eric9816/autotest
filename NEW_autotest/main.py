09a7b46c25809d5276ab343b270284acf9ff6ca3
Eric <eric981623@gmail.com>
2022-06-02 14:32:44 +0300

    Обновил fluid_data

--- NEW_autotest/main.py
+++ NEW_autotest/main.py
@@ -13,7 +13,6 @@ pars_limits = {'wct': [0.0, 0.99],
                'q_fluid': [1/86400, 500/86400],
                'rp': [1, 1000],
                'd': [0.06, 0.1],
-               # 't_res': [280.15, 380.15],
                'gamma_oil': [0.6, 0.8]}
 
 limited_pars = {'d': 'tubing',
@@ -34,28 +33,28 @@ table_model_data = {
     'rw': None,
     'stog': None
 }
-fluid_data = {"q_fluid": 9.94994 / 86400, "wct": 0.00001,
-              "pvt_model_data": {"black_oil": {"gamma_gas": 0.6, "gamma_wat": 1, "gamma_oil": 0.668,
-                                               "rp": 0.01,
-                                               # "oil_correlations": {"pb": "Standing", "rs": "Standing",
-                                               #                      "rho": "Standing",
-                                               #                      "b": "Standing", "mu": "Beggs",
-                                               #                      "compr": "Vasquez", "hc": "const"},
-                                               # "gas_correlations":
-                                               #     {"ppc": "Standing", "tpc": "Standing", "z": "Standing",
-                                               #      "mu": "Lee", "hc": "const"},
-                                               # "water_correlations": {"b": "McCain", "compr": "Kriel",
-                                               #                        "rho": "Standing",
-                                               #                        "mu": "McCain", "hc": "const"},
-                                               # "rsb": {"value": 300, "p": 10000000, "t": 303.15},
-                                               # "muob": {"value": 0.5, "p": 10000000, "t": 303.15},
-                                               # "bob": {"value": 1.5, "p": 10000000, "t": 303.15},
-                                               "table_model_data": table_model_data, "use_table_model": False}}}
+
+fluid_data = {"q_fluid": 9.94994 / 86400,
+              "pvt_model_data": {"black_oil": {"gamma_gas": 0.7, "gamma_wat": 1, "gamma_oil": 0.8,
+                                "wct": 0.5, "phase_ratio": {"type": "GOR", "value": 500},
+                                "oil_correlations":
+                                    {"pb": "Standing", "rs": "Standing",
+                                     "rho": "Standing", "b": "Standing",
+                                     "mu": "Beggs", "compr": "Vasquez"},
+                                "gas_correlations": {"ppc": "Standing", "tpc": "Standing",
+                                                     "z": "Dranchuk", "mu": "Lee"},
+                                "water_correlations": {"b": "McCain", "compr": "Kriel",
+                                                       "rho": "Standing", "mu": "McCain"},
+                                "rsb": {"value": 50, "p": 10000000, "t": 303.15},
+                                "muob": {"value": 0.5, "p": 10000000, "t": 303.15},
+                                "bob": {"value": 1.5, "p": 10000000, "t": 303.15},
+                                "table_model_data": None, "use_table_model": False,
+                                "table_mu": None}}}
 
 ambient_temperature_data = {'MD': [0, 1800], 'T': [293.15, 313.15]}
 amb_temp = AmbientTemperatureDistribution(ambient_temperature_data)
 
-calc_type = 'pipe'
+calc_type = 'pvt'
 # -------------------------------------------------------------------------------------------
 # для pipe
 equipment_data = {'packer': True}
@@ -72,13 +71,13 @@ pipe_data = {"casing": {"bottom_depth": 1800,
                         "s_wall": 0.005}}
 
 # -------------------------------------------------------------------------------------------
-file_path = "C:/Users/PC/PycharmProjects/pythonProject/tuffp/1aa tuffp test 1 29.05.xlsx"
+file_path = "C:/Users/PC/PycharmProjects/pythonProject/tuffp/beggsbrill_test.xlsx"
 model_path = 'C:/Users/PC/PycharmProjects/pythonProject/pips/500.pips'
 
 pfl = 10
 calc_options = {'error_calc': True,
                 'save_results': False,
-                'plot_results': False,
+                'plot_results': True,
                 'scenario': False}
 
 autotest_core.calc_autotest(p_atma=pfl,
